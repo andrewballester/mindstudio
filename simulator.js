59a41030f037fcac0a251014f1ab0db7ca65429b
andrewballester <andrewballester@gmail.com>
2024-03-28 15:41:33 -0700

    first commit

---
+++ simulator.js
@@ -0,0 +1,12 @@
+environment = {
+    vars: {
+      // Session variables set by other blocks
+      myName: 'Andy',
+    },
+    config: {
+      // Variables configured by block settings
+      eyepopai_popuuid: '36ff3b2603f544f7a4123c6cd1108b28',
+      eyepopai_apiKey: 'AAGtYFLxaec5VdXq890PtZkGZ0FBQUFBQm1CZUJBUXJuXzJSQjZNc2xzV1o5UEZqS3l1ak54aTcxY1FxRng4NUtOaW9LaHJ1ejdKaHJ5dFRGYkxfNDdTR2ZDekFkT1diQkZwSlJLTmZhY2tmNkxKUkl3YS1nWjNfOXRoWkVYRW1UTHlNWkdiemM9',
+      eyepopai_url: 'https://images.classmates.com/pho202/z/W/e/6/6724296_display.jpg'
+    },
+  }
